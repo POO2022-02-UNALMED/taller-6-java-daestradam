06af82bf5a31d6980cca23818528f25b4c9172d1
Daniel Estrada <daestradam@unal.edu.co>
2022-10-01 22:20:21 -0500

    1

--- src/main/java/vehiculos/Camion.java
+++ src/main/java/vehiculos/Camion.java
@@ -1,7 +1,27 @@
 package vehiculos;
 
-import Vehiculo;
-
 public class Camion extends Vehiculo {
+	private int ejes;
+	private static int cantidadCamiones=0;
+	
+	public Camion(String placa, String nombre, int precio, int peso, Fabricante fabricante) {
+		super(placa, 2, 80, nombre, precio, peso, "4X2", fabricante);
+		cantidadCamiones++;
+	}
+	
+	public int getEjes() {
+		return ejes;
+	}
+	
+	public void setEjes(int ejes) {
+		this.ejes = ejes;
+	}
 
+	public static int getCantidadCamiones() {
+		return cantidadCamiones;
+	}
+
+	public static void setCantidadCamiones(int cantidadCamiones) {
+		Camion.cantidadCamiones = cantidadCamiones;
+	}
 }
--- src/main/java/vehiculos/Camioneta.java
+++ src/main/java/vehiculos/Camioneta.java
@@ -1,7 +1,27 @@
 package vehiculos;
 
-import Vehiculo;
-
 public class Camioneta extends Vehiculo {
+	private Boolean volco;
+	private static int cantidadCamionetas=0;
+	
+	public Camioneta(String placa, int puertas, String nombre, int precio, int peso, Fabricante fabricante) {
+		super(placa, puertas, 90, nombre, precio, peso, "4X4", fabricante);
+		cantidadCamionetas++;
+	}
+	
+	public Boolean getVolco() {
+		return volco;
+	}
+	
+	public void setVolco(Boolean volco) {
+		this.volco = volco;
+	}
 
+	public static int getCantidadCamionetas() {
+		return cantidadCamionetas;
+	}
+
+	public static void setCantidadCamionetas(int cantidadCamionetas) {
+		Camioneta.cantidadCamionetas = cantidadCamionetas;
+	}
 }
