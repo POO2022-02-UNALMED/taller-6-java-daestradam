fbe12dfd434e9324a0c000ee7b0f0bf69867d6db
Daniel Estrada <daestradam@unal.edu.co>
2022-10-01 22:29:10 -0500

    3

--- src/main/java/vehiculos/Automovil.java
+++ src/main/java/vehiculos/Automovil.java
@@ -4,8 +4,9 @@ public class Automovil extends Vehiculo {
 	private int puestos;
 	private static int cantidadAutomoviles=0;
 	
-	public Automovil(String placa, String nombre, int precio, int peso, Fabricante fabricante){
+	public Automovil(String placa, String nombre, int precio, int peso, Fabricante fabricante, int puestos){
 		super(placa, 4, 100, nombre, precio, peso, "FWD", fabricante);
+		this.puestos=puestos;
 		cantidadAutomoviles++;
 	}
 	
