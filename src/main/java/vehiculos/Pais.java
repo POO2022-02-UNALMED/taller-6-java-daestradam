bcb51be23179fcc34bdf0637fd5df82ba2dfb512
Daniel Estrada <daestradam@unal.edu.co>
2022-10-01 22:23:22 -0500

    a

--- src/main/java/vehiculos/Pais.java
+++ src/main/java/vehiculos/Pais.java
@@ -2,7 +2,11 @@ package vehiculos;
 
 public class Pais {
 	private String nombre;
-
+	
+	public Pais(String nombre) {
+		this.nombre=nombre;
+	}
+	
 	public String getNombre() {
 		return nombre;
 	}
